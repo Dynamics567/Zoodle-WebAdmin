7d17087eb796ed832ccbd72e5bbf4f5486324bcd
Otagera <otagera@gmail.com>
2022-01-07 15:53:59 +0100

    Removed unnecesary stuff

--- src/components/AdminNavbar.js
+++ src/components/AdminNavbar.js
@@ -1,17 +1,17 @@
 import { useLocation } from "react-router-dom";
 import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
-import NavbarInput from "@material-tailwind/react/NavbarInput";
-import Image from "@material-tailwind/react/Image";
-import Dropdown from "@material-tailwind/react/Dropdown";
-import DropdownItem from "@material-tailwind/react/DropdownItem";
-import ProfilePicture from "assets/img/team-1-800x800.jpg";
+// import NavbarInput from "@material-tailwind/react/NavbarInput";
+// import Image from "@material-tailwind/react/Image";
+// import Dropdown from "@material-tailwind/react/Dropdown";
+// import DropdownItem from "@material-tailwind/react/DropdownItem";
+// import ProfilePicture from "assets/img/team-1-800x800.jpg";
 
 export default function AdminNavbar({ showSidebar, setShowSidebar }) {
   const location = useLocation().pathname;
 
   return (
-    <nav className="bg-light-blue-500 md:ml-64 py-6 px-3">
+    <nav className="bg-orange-500 md:ml-64 py-6 px-3">
       <div className="container max-w-full mx-auto flex items-center justify-between md:pr-8 md:pl-10">
         <div className="md:hidden">
           <Button
@@ -51,7 +51,7 @@ export default function AdminNavbar({ showSidebar, setShowSidebar }) {
               : location.toUpperCase().replace("/", "")}
           </h4>
 
-          <div className="flex">
+          {/* <div className="flex">
             <NavbarInput placeholder="Search" />
 
             <div className="-mr-4 ml-6">
@@ -73,7 +73,7 @@ export default function AdminNavbar({ showSidebar, setShowSidebar }) {
                 <DropdownItem color="lightBlue">Something Else</DropdownItem>
               </Dropdown>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </nav>
