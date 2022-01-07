a36d68c8b6b0c3ac43723ccf984fc310e6c530ff
Otagera <otagera@gmail.com>
2022-01-07 17:28:19 +0100

    Status Cards

--- src/components/StatusCard.js
+++ src/components/StatusCard.js
@@ -1,17 +1,16 @@
 import Card from "@material-tailwind/react/Card";
 import CardBody from "@material-tailwind/react/CardBody";
 import H4 from "@material-tailwind/react/Heading4";
-import H5 from "@material-tailwind/react/Heading5";
 
 export default function StatusCard({
   title,
   amount,
 }) {
   return (
-    <div className="px-1 mb-10 w-52 md-w-72 lg:w-56 xl:w-60 text-center">
+    <div className="px-1 mb-10 w-40- md-w-72 lg:w-56 xl:w-60 text-center">
       <Card>
         <CardBody>
-          <H5>{title}</H5>
+          <h5 className="font-bold text-xl">{title}</h5>
           <H4 color='orange'>{amount}</H4>
         </CardBody>
       </Card>
