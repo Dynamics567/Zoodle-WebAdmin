f0c1ec76ece1701d703e48161e6386e39f5d5b89
Othniel Agera <otagera@gmail.com>
2022-01-15 11:47:53 +0100

    Sidebar icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,19 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import AdminNavbar from "./AdminNavbar";
 import H6 from "@material-tailwind/react/Heading6";
+import { ReactComponent as Dashboard } from "../assets/svg/dashboard.svg";
+import { ReactComponent as DashboardActive } from "../assets/svg/dashboard-active.svg";
+import { ReactComponent as Orders } from "../assets/svg/orders.svg";
+import { ReactComponent as OrdersActive } from "../assets/svg/orders-active.svg";
+import { ReactComponent as CRM } from "../assets/svg/CRM.svg";
+import { ReactComponent as CRMActive } from "../assets/svg/CRM-active.svg";
+import { ReactComponent as Notifications } from "../assets/svg/notifications.svg";
+import { ReactComponent as NotificationsActive } from "../assets/svg/notifications-active.svg";
+import { ReactComponent as Stats } from "../assets/svg/stats.svg";
+import { ReactComponent as StatsActive } from "../assets/svg/stats-active.svg";
+import { ReactComponent as UserCtrl } from "../assets/svg/user-control.svg";
+import { ReactComponent as UserCtrlActive } from "../assets/svg/user-control-active.svg";
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState("-left-64");
@@ -9,28 +21,54 @@ const Sidebar = () => {
     {
       to: "/",
       title: "Dashboard",
+      icon: {
+        regular: Dashboard,
+        active: DashboardActive,
+      },
     },
     {
       to: "/orders",
       title: "Orders",
+      icon: {
+        regular: Orders,
+        active: OrdersActive,
+      },
     },
     {
       to: "/crm",
       title: "CRM",
+      icon: {
+        regular: CRM,
+        active: CRMActive,
+      },
     },
     {
       to: "/notifications",
       title: "Notifications",
+      icon: {
+        regular: Notifications,
+        active: NotificationsActive,
+      },
     },
     {
       to: "/stats",
       title: "Stats",
+      icon: {
+        regular: Stats,
+        active: StatsActive,
+      },
     },
     {
       to: "/user-control",
       title: "User Control",
+      icon: {
+        regular: UserCtrl,
+        active: UserCtrlActive,
+      },
     },
   ];
+
+  const location = useLocation();
   return (
     <>
       <AdminNavbar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
@@ -60,6 +98,14 @@ const Sidebar = () => {
                       className="flex items-center gap-4 text-sm text-white font-light px-12 py-3 rounded-lg"
                       activeClassName="bg-white text-orange-500 shadow-md"
                     >
+                      {location.pathname.substring(1).toLowerCase() ===
+                        item.title.toLowerCase() ||
+                      (location.pathname.substring(1) === "" &&
+                        item.title === "Dashboard") ? (
+                        <item.icon.active />
+                      ) : (
+                        <item.icon.regular />
+                      )}
                       <h6>{item.title}</h6>
                     </NavLink>
                   </li>
